f4ba98be06a85fcb35858977e80f4d94b90f1788
Jesse Florig <jesseflorig@gmail.com>
2020-10-22 13:27:59 -0400

    refactored app component

--- src/react/App.js
+++ src/react/App.js
@@ -1,35 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { channels } from '../shared/constants';
 const { ipcRenderer } = window; 
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      appName: '',
-      appVersion: '',
-    }
-    ipcRenderer.send(channels.APP_INFO);
-    ipcRenderer.on(channels.APP_INFO, (event, arg) => {
-      ipcRenderer.removeAllListeners(channels.APP_INFO);
-      const { appName, appVersion } = arg;
-      this.setState({ appName, appVersion });
-    });
-  }
+const App = () => {
+  const [appName, setAppName] = React.useState()
+  const [appVersion, setAppVersion] = React.useState()
 
-  render() {
-    const { appName, appVersion } = this.state;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{appName} version {appVersion}</p>
-        </header>
-      </div>
-    );
-  }
+  ipcRenderer.send(channels.APP_INFO);
+  ipcRenderer.on(channels.APP_INFO, (event, arg) => {
+    ipcRenderer.removeAllListeners(channels.APP_INFO);
+    const { appName, appVersion } = arg;
+    setAppName(appName)
+    setAppVersion(appVersion)
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{appName} version {appVersion}</p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
